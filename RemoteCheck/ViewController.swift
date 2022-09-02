74df01196057ab64bccf2794d1783c0423f0955a
jjhkie <83710866+jjhkie@users.noreply.github.com>
2022-09-02 20:43:37 +0900

    Initial Commit

---
+++ RemoteCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemoteCheck
+//
+//  Created by 김진혁 on 2022/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
